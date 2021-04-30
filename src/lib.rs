1a31b726b2e2e338cf5c7f11383eeed982bb8a57
Makoto <makoto@emurasoft.com>
2021-04-29 17:57:19 -0700

    Insertion sort

--- src/lib.rs
+++ src/lib.rs
@@ -16,19 +16,43 @@ where
     }
 }
 
+pub fn insertion_sort(arr: &mut [i32]) {
+    for i in 1..arr.len() {
+        for j in 0..i {
+            if arr[i - j - 1] < arr[i - j] {
+                break;
+            }
+
+            arr.swap(i - j - 1, i - j);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
+
+    macro_rules! f {
+        ( $function:expr ) => {
+            ($function as fn(&mut [i32]), stringify!($function))
+        };
+    }
 
     #[rstest]
     #[case(&[], &[])]
     #[case(&[0], &[0])]
     #[case(&[0, 1], &[0, 1])]
     #[case(&[1, 0], &[0, 1])]
+    #[case(&[0, 1, 0], &[0, 0, 1])]
+    #[case(&[0, 10, 5, 2, 3, 9], &[0, 2, 3, 5, 9, 10])]
     fn test(#[case] arr: &[i32], #[case] expected: &[i32]) {
-        let mut arr_clone = arr.to_vec();
-        selection_sort(&mut arr_clone);
-        assert_eq!(arr_clone, expected);
+        let functions = &[f!(selection_sort), f!(insertion_sort)];
+
+        for (sort_function, name) in functions {
+            let mut arr_clone = arr.to_vec();
+            sort_function(&mut arr_clone);
+            assert_eq!(arr_clone, expected, "function: {}()", name);
+        }
     }
 }
