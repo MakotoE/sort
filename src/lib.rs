27540393f519dd0ebc36f09d49e51aadf299728c
Makoto <makoto@emurasoft.com>
2021-05-03 22:08:48 -0700

    Merge sort

--- src/lib.rs
+++ src/lib.rs
@@ -53,48 +53,44 @@ where
 }
 
 pub fn merge_sort(arr: &mut [i32]) {
+    // Copied from https://chercher.tech/rust/merge-sort-rust
+
     let mut temp = vec![0; arr.len()];
-    let mut length = 1;
-    while length < arr.len() {
-        let mut from = 0;
-        while from < arr.len() {
-            let mid = usize::min(from + length, arr.len());
-            let upper = usize::min(from + 2 * length, arr.len());
+    let mut half_length = 1;
+    while half_length < arr.len() {
+        for i in 0..=arr.len() / (half_length * 2) {
+            let from = i * half_length * 2;
+            let mid = usize::min(from + half_length, arr.len());
+            let upper = usize::min(from + 2 * half_length, arr.len());
             merge(&arr[from..mid], &arr[mid..upper], &mut temp[from..upper]);
             arr[from..upper].copy_from_slice(&temp[from..upper]);
-            from += 2 * length;
         }
 
-        length *= 2;
+        half_length *= 2;
     }
 }
 
 fn merge(left: &[i32], right: &[i32], result: &mut [i32]) {
     let mut left_index = 0;
     let mut right_index = 0;
-    let mut temp_index = 0;
+    let mut result_index = 0;
 
     while left_index < left.len() && right_index < right.len() {
         if left[left_index] < right[right_index] {
-            result[temp_index] = left[left_index];
+            result[result_index] = left[left_index];
             left_index += 1;
         } else {
-            result[temp_index] = right[right_index];
+            result[result_index] = right[right_index];
             right_index += 1;
         }
 
-        temp_index += 1;
+        result_index += 1;
     }
 
-    while left_index < left.len() {
-        result[temp_index] = left[left_index];
-        temp_index += 1;
-        left_index += 1;
-    }
-    while right_index < right.len() {
-        result[temp_index] = right[right_index];
-        temp_index += 1;
-        right_index += 1;
+    if left_index < left.len() {
+        result[result_index..].copy_from_slice(&left[left_index..]);
+    } else if right_index < right.len() {
+        result[result_index..].copy_from_slice(&right[right_index..]);
     }
 }
 
@@ -117,16 +113,17 @@ mod tests {
     #[case(&[0, 1, 0], &[0, 0, 1])]
     #[case(&[0, 10, 5, 2, 3, 9], &[0, 2, 3, 5, 9, 10])]
     fn test(#[case] arr: &[i32], #[case] expected: &[i32]) {
-        // let functions = &[f!(selection_sort), f!(insertion_sort), f!(shell_sort)];
-        //
-        // for (sort_function, name) in functions {
-        //     let mut arr_clone = arr.to_vec();
-        //     sort_function(&mut arr_clone);
-        //     assert_eq!(arr_clone, expected, "function: {}()", name);
-        // }
-        let mut arr_clone = arr.to_vec();
-        let length = arr_clone.len();
-        merge_sort(&mut arr_clone);
-        assert_eq!(arr_clone, expected);
+        let functions = &[
+            f!(selection_sort),
+            f!(insertion_sort),
+            f!(shell_sort),
+            f!(merge_sort),
+        ];
+
+        for (sort_function, name) in functions {
+            let mut arr_clone = arr.to_vec();
+            sort_function(&mut arr_clone);
+            assert_eq!(arr_clone, expected, "function: {}()", name);
+        }
     }
 }
